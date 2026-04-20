e85f02957c25528e56e98784e9d621d041547e73
Manish Parihar <manishparihar3618@gmail.com>
2026-04-20 23:32:58 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -3,7 +3,7 @@ import pandas as pd
 def load_data(filepath="../data/students_data.csv"
 ):
 
-    student_df = pd.read_csv(filepath)
+   student_df = pd.read_csv(filepath)
     return student_df
 
 
